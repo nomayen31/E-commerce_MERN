2f88a42613aa9ba6323f30b19afcbee48bb030dd
nomayen <nomayen.ohin@gmail.com>
2023-06-01 23:11:43 +0600

    response handler

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,37 +1,49 @@
 const createError = require('http-errors');
 const User = require('../models/userModels');
-const getUsers = async (req, res, next)=>{
-   try {
-    const search = req.query.search || "";
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 5;
+const { successResponse } = require('./responseController');
+const getUsers = async (req, res, next) => {
+    try {
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 1;
 
-    const searchRegExp = new RegExp('.*' + search + ".*", 'i');
+        const searchRegExp = new RegExp('.*' + search + ".*", 'i');
 
-    const filter = {
-        isAdmin:{ $ne: true},
-        $or:[
-            {name: {$regex: searchRegExp}},
-            {email: {$regex: searchRegExp}},
-            {phone: {$regex: searchRegExp}}
-        ]
-    };
+        const filter = {
+            isAdmin: { $ne: true },
+            $or: [
+                { name: { $regex: searchRegExp } },
+                { email: { $regex: searchRegExp } },
+                { phone: { $regex: searchRegExp } }
+            ]
+        };
 
-    const options = {password: 0}
+        const options = { password: 0 }
+        const users = await User.find(filter, options)
+            .limit(limit)
+            .skip((page - 1) * limit)
+        const count = await User.find(filter).countDocuments();
+
+        if (!users) throw createError(404, "no user found");
 
 
+        return successResponse(res, {
+            statusCode: 200,
+            message: 'User were is returned successfully',
+            payload: {
+                users,
+                pagination: {
+                    totalPages: Math.ceil(count / limit),
+                    currentPage: page,
+                    previousPage: page - 1 > 0 ? page - 1 : null,
+                    nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+                }
+            }
+        })
 
-    const users = await User.find(filter,options)
-    .limit(limit)
-    .skip((page-1)*limit)
-    res.status(200).send({
-        message:'User were is return',
-        users,
-    
-    });
-   } catch (error) {
+    } catch (error) {
         next(error);
-   }
+    }
 };
 
-module.exports = {getUsers}
+module.exports = { getUsers }
